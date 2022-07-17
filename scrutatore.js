e6cbd03dcc983682ae5bb49a4335bc11d3c85c52
4utotune <71821593+4utotune@users.noreply.github.com>
2022-07-18 01:28:23 +0200

    bozza continuo

---
+++ scrutatore.js
@@ -0,0 +1,12 @@
+let i=0;
+
+function prova(){
+    if(i==0)
+    {
+        alert('Non è possibile inserire i risultati dello scrutionio prima che le elezioni finiscano');
+        i++;
+    }else{
+        window.open("https://www.google.com", "_self"); 
+        i=0;
+    }
+}
